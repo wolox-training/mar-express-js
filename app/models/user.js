a267d175ca5cfea3c54de2963ae0a091d1629551
Mariano Ranucci <mranucci@est.frba.utn.edu.ar>
2019-12-16 09:46:06 -0300

    Applied changes to user model and migration

--- app/models/user.js
+++ app/models/user.js
@@ -3,10 +3,10 @@ module.exports = (sequelize, DataTypes) => {
     'users',
     {
       id: { type: DataTypes.INTEGER, primaryKey: true },
-      firstName: { type: DataTypes.STRING },
-      lastName: { type: DataTypes.STRING },
-      email: { type: DataTypes.STRING },
-      password: { type: DataTypes.STRING }
+      first_name: { type: DataTypes.STRING, allowNull: false },
+      last_name: { type: DataTypes.STRING, allowNull: false },
+      email: { type: DataTypes.STRING, allowNull: false },
+      password: { type: DataTypes.STRING, allowNull: false }
     },
     {
       timestamps: true
--- migrations/migrations/20191204134552-create_users_table.js
+++ migrations/migrations/20191204134552-create_users_table.js
@@ -2,29 +2,14 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) =>
-    queryInterface.createTable(
-      'users',
-      {
-        id: {
-          type: Sequelize.INTEGER,
-          primaryKey: true,
-          autoIncrement: true
-        },
-        createdAt: {
-          type: Sequelize.DATE
-        },
-        updatedAt: {
-          type: Sequelize.DATE
-        },
-        firstName: { type: Sequelize.STRING, allowNull: false },
-        lastName: { type: Sequelize.STRING, allowNull: false },
-        email: { type: Sequelize.STRING, allowNull: false, unique: true },
-        password: { type: Sequelize.STRING, allowNull: false }
-      },
-      {
-        engine: 'MYISAM',
-        charset: 'latin1',
-        schema: 'public'
-      }
-    )
+    queryInterface.createTable('users', {
+      id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
+      created_at: { type: Sequelize.DATE, allowNull: false },
+      updated_at: { type: Sequelize.DATE, allowNull: false },
+      deleated_at: { type: Sequelize.DATE, allowNull: false },
+      first_name: { type: Sequelize.STRING, allowNull: false },
+      last_name: { type: Sequelize.STRING, allowNull: false },
+      email: { type: Sequelize.STRING, allowNull: false, unique: true },
+      password: { type: Sequelize.STRING, allowNull: false }
+    })
 };
