44420bf3a72496d44d3158230958ab8d8b5c24db
Mariano Ranucci <mranucci@est.frba.utn.edu.ar>
2019-12-19 13:05:33 -0300

    Applied corrections to sign up controller

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -6,41 +6,43 @@ const util = require('util');
 const User = require('../models').users;
 const logger = require('../logger');
 const error = require('../errors');
+const config = require('../../config/index');
 
 const { userCreationError } = error;
-const saltRounds = 10;
+const { saltRounds } = config.common.bcrypt;
 
 const validateParams = (email, password) =>
   new Promise((resolve, reject) => {
     if (isAlphanumeric(password) && password.length >= 8 && email.split('@')[1] === 'wolox.com.ar') {
       resolve('Valid Params');
     } else {
-      reject(Error('Invalid params!'));
+      reject(userCreationError('Invalid params!'));
     }
   });
 
-const createUser = req => {
+const hashPassword = password => bcrypt.hash(password, saltRounds);
+
+const createUser = (firstName, lastName, email, password) =>
+  User.create({ firstName, lastName, email, password });
+
+exports.postUser = (req, res, next) => {
   const { first_name: firstName, last_name: lastName, email, password } = req.body;
   return validateParams(email, password)
-    .then(
-      bcrypt
-        .hash(password, saltRounds)
-        .then(hash => {
-          User.create({ firstName, lastName, email, password: hash });
-          logger.info(`Usuario creado para: ${firstName} ${lastName}`);
-        })
+    .then(() =>
+      hashPassword(password)
+        .then(() =>
+          createUser(firstName, lastName, email, password)
+            .then(logger.info(`Usuario creado para: ${firstName} ${lastName}`))
+            .catch(err => {
+              logger.error(util.inspect(err));
+              throw userCreationError(err.message);
+            })
+        )
         .catch(err => {
           logger.error(util.inspect(err));
           throw userCreationError(err.message);
         })
     )
-    .catch(err => {
-      logger.error(util.inspect(err));
-      throw userCreationError(err.message);
-    });
-};
-
-exports.postUser = (req, res, next) =>
-  createUser(req)
     .then(response => res.status(201).send(response.body))
     .catch(next);
+};
