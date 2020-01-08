2d84eba72e772ed048f7bb3d845e63690df248b5
Mariano Ranucci <mranucci@est.frba.utn.edu.ar>
2020-01-08 11:09:50 -0300

    Modified user validations

--- app/middlewares/user_validation.js
+++ app/middlewares/user_validation.js
@@ -1,14 +1,45 @@
-const isAlphanumeric = require('is-alphanumeric');
+const { checkSchema, validationResult } = require('express-validator');
 
 const error = require('../errors');
 
 const { userCreationError } = error;
 
-exports.validateParams = (req, res, next) => {
-  const { email, password } = req.body;
-  if (isAlphanumeric(password) && password.length >= 8 && email.split('@')[1] === 'wolox.com.ar') {
-    next();
-  } else {
-    next(userCreationError('Invalid params!'));
+exports.userValidationRules = () => [
+  checkSchema({
+    firstName: {
+      notEmpty: true,
+      errorMessage: "firstName can't be blank"
+    },
+    lastName: {
+      notEmpty: true,
+      errorMessage: "lastName can't be blank"
+    },
+    email: {
+      notEmpty: true,
+      matches: {
+        options: /@(wolox)\.com\.ar$/i,
+        errorMessage: 'you may only use email addresses from wolox domain'
+      }
+    },
+    password: {
+      isLength: {
+        errorMessage: 'Password should be at least 8 characters long',
+        options: { min: 8 }
+      },
+      isAlphanumeric: {
+        errorMessage: 'Password should be alphanumeric'
+      }
+    }
+  })
+];
+
+exports.validateUser = (req, res, next) => {
+  const errors = validationResult(req);
+  if (errors.isEmpty()) {
+    return next();
   }
+  const extractedErrors = [];
+  errors.array().map(err => extractedErrors.push({ [err.param]: err.msg }));
+
+  return next(userCreationError(extractedErrors));
 };
--- app/routes.js
+++ app/routes.js
@@ -1,11 +1,11 @@
 const { healthCheck } = require('./controllers/healthCheck');
 const { getAlbums, getAlbumPhotos } = require('./controllers/albums');
 const { postUser } = require('./controllers/users');
-const { validateParams } = require('./middlewares/user_validation');
+const { userValidationRules, validateUser } = require('./middlewares/user_validation');
 
 exports.init = app => {
   app.get('/health', healthCheck);
   app.get('/albums', getAlbums);
   app.get('/albums/:id/photos', getAlbumPhotos);
-  app.post('/users', [validateParams], postUser);
+  app.post('/users', [userValidationRules(), validateUser], postUser);
 };
